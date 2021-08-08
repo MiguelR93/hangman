d9b2679cc4e5455941b09e393cf86089fe0fa7d4
Miguel <miguelromerop93@gmail.com>
2021-08-08 18:07:50 -0500

    creación de un diccionario para poder elegir aleatoriamente una palabra

---
+++ hangmang_game/juego_del_ahorcado.py
@@ -0,0 +1,12 @@
+def run():
+    data = []
+    with open("./data.txt", "r", encoding="utf-8") as word:
+        for i in word:
+            data.append(str(i))
+    print(data)
+    origin = {n:data[n] for n in range(len(data))}
+    print(origin)
+
+
+if __name__ == "__main__":
+    run()    
