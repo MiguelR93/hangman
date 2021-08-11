0ab418f3c115e868d832422bfae87dcefee4f088
Miguel <miguelromerop93@gmail.com>
2021-08-11 14:23:49 -0500

    adición de notas en el ahorcado

--- hangmang_game/juego_del_ahorcado.py
+++ hangmang_game/juego_del_ahorcado.py
@@ -1,8 +1,11 @@
 import random, os
 
 def run():
+    # # limpiando la pantalla
     os.system("clear")
+    # # creando una lista que contendrá las palabras del documento "data.txt"
     data = []
+    # # extrayendo las palabras de "data.txt"
     with open("./data.txt", "r", encoding="utf-8") as word:
         for i in word:
             width = len(i) - 1
@@ -10,15 +13,16 @@ def run():
     # print(data)
     origin = {n:data[n] for n in range(len(data))}
     # print(origin)
-    # elegir una palabra aleatoriamente desde origin:
+    # # elegir una palabra aleatoriamente desde origin:
     # print(len(data))
     alea = random.randint(0,len(data))
     elegida = origin[alea]
     # print(alea)
+    # # imprimir los espacios que ocupa la palabra y el mensaje de interacción con el usuario
     input(f"""{len(elegida)*"_ "}\n\nIngresa una letra: """)
-
-    
+    # # pedirle al usuario que ingrese una letra hasta que adivine    
 
 
 if __name__ == "__main__":
+    # # inicio
     run()
