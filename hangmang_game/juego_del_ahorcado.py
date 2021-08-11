1999ff8afaf2b61b7131ecd04d7532d12644c6fa
Miguel <miguelromerop93@gmail.com>
2021-08-11 00:53:40 -0500

    adición del módulo os adición de un input para que el usuario agregue su letra [no terminado]

--- hangmang_game/juego_del_ahorcado.py
+++ hangmang_game/juego_del_ahorcado.py
@@ -1,13 +1,24 @@
+import random, os
+
 def run():
+    os.system("clear")
     data = []
     with open("./data.txt", "r", encoding="utf-8") as word:
         for i in word:
             width = len(i) - 1
             data.append(i[:width])
-    print(data)
+    # print(data)
     origin = {n:data[n] for n in range(len(data))}
-    print(origin)
+    # print(origin)
+    # elegir una palabra aleatoriamente desde origin:
+    # print(len(data))
+    alea = random.randint(0,len(data))
+    elegida = origin[alea]
+    # print(alea)
+    input(f"""{len(elegida)*"_ "}\n\nIngresa una letra: """)
+
+    
 
 
 if __name__ == "__main__":
-    run()    
+    run()
