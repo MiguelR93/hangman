9a3ae2538a9eadf274551e9cde3dfd2084227506
MiguelR93 <miguelromerop93@gmail.com>
2021-08-23 17:50:20 -0500

    cleaning the code

--- hangman.py
+++ hangman.py
@@ -105,38 +105,24 @@ def dibu(OPORTUNIDADES):
     elif OPORTUNIDADES == 0:
         print(cero)
 
+
 def run():
-    # creando una lista que contendrá las palabras del documento "data.txt"
-    data = []
-    # extrayendo las palabras de "data.txt"
-    with open("./data.txt", "r", encoding="utf-8") as word:
+    data = [] # it creates a list with words from data.txt // crea una lista que contendrá las palabras del documento "data.txt
+    with open("./data.txt", "r", encoding="utf-8") as word: # It opens the data.txt document and gets words form it // extrayendo las palabras de "data.txt"
         for i in word:
             width = len(i) - 1
             data.append(i[:width])
 
 
-    origin = {n:data[n] for n in range(len(data))}
+    origin = {n:data[n] for n in range(len(data))} # dictionary with words from data // diccionario con palabras de data.txt
+    elegidaNato = origin[random.randint(0,len(data))] # chose a random word from origin // elegir una palabra aleatoriamente desde origin:
+    elegida = elegidaNato.replace("á",'a').replace('é','e').replace('é','e').replace('í','i').replace('ó','o').replace('ú','u').upper()
+    descubierto = {i:"_" for i in range(len(elegida))} # store words revelated by the player // guarda lo que el jugador va develando
+    letrasAdmitidas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] # valid characters
+    palabra = {i:a for i,a in enumerate(elegida)} # It creates a dictionary with each character of the chosen word
+    letrasUsadas = [] # store valid characters used by the player
 
-    # elegir una palabra aleatoriamente desde origin:
-    elegidaNato = origin[random.randint(0,len(data))]
-    elegidaA = elegidaNato.replace("á",'a')
-    elegidaE = elegidaA.replace('é','e')
-    elegidaI = elegidaE.replace('í','i')
-    elegidaO = elegidaI.replace('ó','o')
-    elegidaU = elegidaO.replace('ú','u')
-    elegida = elegidaU.upper()
-
-    # guarda lo que el jugador va develando:
-    descubierto = {i:"_" for i in range(len(elegida))}
-
-    letrasAdmitidas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-    palabra = {i:a for i,a in enumerate(elegida)}
-
-    letrasUsadas = []
-
-    # contador de oportunidades:
-    OPORTUNIDADES = 6
+    OPORTUNIDADES = 6 # chance's counter // contador de oportunidades
 
     # Game begin
     while OPORTUNIDADES > 0 and palabra != descubierto:
@@ -147,8 +133,7 @@ def run():
         for i in descubierto.values():
             print(i, end=" ")
         letra = input("""\n\nIngresa una letra: """)
-        # evaluando que lo registrado sea una letra:
-        CONTADORDELETRASADMITIDAS = 0
+        CONTADORDELETRASADMITIDAS = 0 # Counter for valid characters in every loop; it must be lower than 27
         for i in letrasAdmitidas:
             if letra != i:
                 CONTADORDELETRASADMITIDAS += 1
@@ -157,8 +142,7 @@ def run():
             input("persiona enter")
             continue
         
-        LETRAREPETIDA = 0
-        # la letra ya fue usada?
+        LETRAREPETIDA = 0 # It increase if the characters has been used before
         for i in letrasUsadas:
             if i == letra:
                 print("esta letra ya fue usada")
@@ -166,8 +150,7 @@ def run():
                 LETRAREPETIDA += 1
                 break
         
-        # reiniciar si ya se usó la letra:
-        if LETRAREPETIDA > 0:
+        if LETRAREPETIDA > 0: # It restart the loop if the character has been used before // reiniciar si ya se usó la letra
             continue
         else:
             for i,a in palabra.items():
