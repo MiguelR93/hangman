dbcc85fa264447cac0dcf39a84c0ba9267d380b2
MR <miguelromerop93@gmail.com>
2021-09-25 17:29:24 -0500

    changing data types

--- hangman.py
+++ hangman.py
@@ -91,7 +91,7 @@ salvado = """
 
 data = [] # it creates a list with words from data.txt // crea una lista que contendrá las palabras del documento "data.txt
 origin = {} # dictionary with words from data // diccionario con palabras de data.txt
-elegidaNato = 0 # chose a random word from origin // elegir una palabra aleatoriamente desde origin:
+elegidaNato = [] # chose a random word from origin // elegir una palabra aleatoriamente desde origin:
 elegida = 0 # modifies the word to make it easier to understand for the program
 descubierto = 0 # store words revelated by the player // guarda lo que el jugador va develando
 letrasAdmitidas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] # valid characters
@@ -121,26 +121,18 @@ def openData():
             width = len(i) - 1 # evalueta number of characters of each taken word
             data.append(i[:width]) # adds the taken word to data
 
-# def makeWordList():
-    # global origin
-    # origin = {n:data[n] for n in range(len(data))}
-    # elegidaNato = origin[random.randint(0,len(data))]
-    # elegida = elegidaNato.replace("á",'a').replace('é','e').replace('é','e').replace('í','i').replace('ó','o').replace('ú','u').upper()
-    # descubierto = {i:"_" for i in range(len(elegida))}
-    # palabra = {i:a for i,a in enumerate(elegida)}
-    # return origin, elegidaNato, elegida, descubierto, palabra
+def makingOrigin():
+    # make the origin dictionary:
+    for i in range(len(data)):
+        origin[i] = data[i]
 
 def run():
-    # data = [] # it creates a list with words from data.txt // crea una lista que contendrá las palabras del documento "data.txt
-    # with open("./data.txt", "r", encoding="utf-8") as word: # It opens the data.txt document and gets words form it // extrayendo las palabras de "data.txt"
-    #     for i in word:
-    #         width = len(i) - 1 # evalueta number of characters of each taken word
-    #         data.append(i[:width]) # adds the taken word to data
     openData()
-    # makeWordList()
-    origin = {n:data[n] for n in range(len(data))}
-    elegidaNato = origin[random.randint(0,len(data))]
-    elegida = elegidaNato.replace("á",'a').replace('é','e').replace('é','e').replace('í','i').replace('ó','o').replace('ú','u').upper()
+    makingOrigin()
+    # origin = {n:data[n] for n in range(len(data))}
+    Nato = origin[random.randint(0,len(data))]
+    elegidaNato.append(Nato)
+    elegida = elegidaNato[0].replace("á",'a').replace('é','e').replace('é','e').replace('í','i').replace('ó','o').replace('ú','u').upper()
     descubierto = {i:"_" for i in range(len(elegida))}
     palabra = {i:a for i,a in enumerate(elegida)}
 
